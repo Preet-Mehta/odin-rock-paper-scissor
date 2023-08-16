6bb75f3372d7b79fbcdf91656ab6084061b64071
Preet Mehta <mehtapreet404@gmail.com>
2023-08-16 09:34:32 +0530

    Added UI to the game.

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const inputs = ["rock", "paper", "scissors"];
+let pScore = 0;
+let cScore = 0;
+let ties = 0;
+
+function disableButton(button) {
+  button.disabled = true;
+  button.style.cursor = "no-drop";
+}
+
+function enableButton(button) {
+  button.disabled = false;
+  button.style.cursor = "pointer";
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 function getInputString() {
   return inputs.join(" ");
@@ -9,90 +26,117 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  const returnObj = { error: true, pWon: false, tie: false, message: "" };
+  const returnObj = { pWon: false, tie: false, message: "" };
 
-  const pSelection = playerSelection.toLowerCase();
-  const cSelection = computerSelection.toLowerCase();
-
-  if (!inputs.includes(pSelection)) {
-    returnObj.message = `Incorrect input found. Choice must be from: ${getInputString()}`;
-    return returnObj;
-  }
-
-  returnObj.error = false;
-
-  if (pSelection === cSelection) {
+  if (playerSelection === computerSelection) {
     returnObj.message = "Tied! Nothing won, nothing lost.";
     returnObj.tie = true;
     return returnObj;
   }
 
   if (
-    (pSelection === "rock" && cSelection === "scissors") ||
-    (pSelection === "paper" && cSelection === "rock") ||
-    (pSelection === "scissors" && cSelection === "paper")
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
   ) {
     returnObj.pWon = true;
-    returnObj.message = `You win! ${pSelection} beats ${cSelection}.`;
+    returnObj.message = `You win! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(
+      computerSelection
+    )}.`;
     return returnObj;
   }
 
-  returnObj.message = `You lose! ${cSelection} beats ${pSelection}.`;
+  returnObj.message = `You lose! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(
+    playerSelection
+  )}.`;
   return returnObj;
 }
 
 function generateResult(pScore, cScore) {
+  disableButton(rockButton);
+  disableButton(paperButton);
+  disableButton(scissorsButton);
+
   if (pScore === cScore) {
-    console.log(`Match tied! Both won ${pScore} rounds.`);
+    finalStatusMsg.textContent = `Match tied! Both won ${pScore} rounds.`;
+    finalStatusMsg.style.color = "blue";
   } else if (pScore > cScore) {
-    console.log(`You won! Rounds won: ${pScore}. Rounds lost: ${cScore}. Rounds tied: ${5 - pScore - cScore}`);
+    finalStatusMsg.textContent = `You won! Rounds won: ${pScore}. Rounds lost: ${cScore}. Rounds tied: ${ties}`;
+    finalStatusMsg.style.color = "green";
   } else {
-    console.log(`You lost! Rounds won: ${pScore}. Rounds lost: ${cScore}. Rounds tied: ${5 - pScore - cScore}`);
+    finalStatusMsg.textContent = `You lost! Rounds won: ${pScore}. Rounds lost: ${cScore}. Rounds tied: ${ties}`;
+    finalStatusMsg.style.color = "red";
   }
+
+  playAgainButton.style.display = "";
 }
 
-function game() {
-  let pScore = 0;
-  let cScore = 0;
-  let retries = 0;
-  let rounds = 0;
+function game(e) {
+  const computerSelection = getComputerChoice();
+  const playerSelection = e.target.classList[0];
 
-  while (rounds < 5) {
-    const playerSelection = prompt(`What's your choice: ${getInputString()}`);
-    const computerSelection = getComputerChoice();
+  const roundStatus = playRound(playerSelection, computerSelection);
+  roundStatusMsg.textContent = roundStatus.message;
 
-    const roundStatus = playRound(playerSelection, computerSelection);
-
-    if (roundStatus.error) {
-      console.log(roundStatus.message);
-      if (retries === 2) {
-        break;
-      }
-
-      console.log(`${3 - ++retries} attempt(s) remaining before the game will quit.`);
-      continue;
-    }
-
-    rounds++;
-
-    if (roundStatus.tie) {
-      console.log(roundStatus.message);
-      continue;
-    }
-
-    console.log(roundStatus.message);
-    if (roundStatus.pWon) {
-      pScore++;
-    } else {
-      cScore++;
-    }
+  if (roundStatus.tie) {
+    roundStatusMsg.style.color = "blue";
+    ties++;
+    return;
   }
 
-  if (retries < 2) {
+  if (roundStatus.pWon) {
+    roundStatusMsg.style.color = "green";
+    pScore++;
+  } else {
+    roundStatusMsg.style.color = "red";
+    cScore++;
+  }
+
+  pScoreElement.textContent = pScore;
+  cScoreElement.textContent = cScore;
+
+  if (cScore === 5 || pScore === 5) {
     generateResult(pScore, cScore);
-  } else {
-    console.log("Too many incorrect attempts recorded.");
   }
 }
 
-game();
+function resetGame() {
+  // Reset buttons.
+  playAgainButton.style.display = "none";
+  enableButton(rockButton);
+  enableButton(paperButton);
+  enableButton(scissorsButton);
+
+  // Reset scores.
+  cScore = 0;
+  pScore = 0;
+  ties = 0;
+
+  pScoreElement.textContent = pScore;
+  cScoreElement.textContent = cScore;
+
+  // Reset messages.
+  roundStatusMsg.textContent = "";
+  finalStatusMsg.textContent = "";
+}
+
+// Target HTML Elements
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+const playAgainButton = document.querySelector("#play-again");
+
+const pScoreElement = document.querySelector("#player-score");
+const cScoreElement = document.querySelector("#comp-score");
+
+const roundStatusMsg = document.querySelector(".round-status");
+const finalStatusMsg = document.querySelector(".final-status");
+
+// Event Listeners
+rockButton.addEventListener("click", game);
+paperButton.addEventListener("click", game);
+scissorsButton.addEventListener("click", game);
+playAgainButton.addEventListener("click", resetGame);
+
+// Initial state.
+resetGame();
